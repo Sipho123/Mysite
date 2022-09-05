0e6497d625b25d813df9a85edb049a82d1a1e9a8
Sipho123 <58542277+Sipho123@users.noreply.github.com>
2022-09-05 12:13:13 +0300

    Update admin.py
    Unused import deleted
    from .models import *
    
    admin.site.register(Admin)
    admin.site.register(Customer)
    admin.site.register(Category)
    admin.site.register(Product)
    admin.site.register(CartProduct)
    admin.site.register(Cart)
    admin.site.register(Order)
    admin.site.register(ProductImage)

--- our_store/admin.py
+++ our_store/admin.py
@@ -1,13 +1,4 @@
 from django.contrib import admin
 
-from .models import *
 
-admin.site.register(Admin)
-admin.site.register(Customer)
-admin.site.register(Category)
-admin.site.register(Product)
-admin.site.register(CartProduct)
-admin.site.register(Cart)
-admin.site.register(Order)
-admin.site.register(ProductImage)
 
